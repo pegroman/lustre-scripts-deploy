4f380cbe1a99cfa65ed79d9fc4a6ff8942b70c62
pegroman <roman.alejandro.b@gmail.com>
2024-03-31 21:27:17 -0300

    corriegiendo doble install en depen.sh

--- depen.sh
+++ depen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dnf install -y asciidoc audit-libs-devel automake bc binutils-devel bison elfutils-devel elfutils-libelf-devel expect flex gcc gcc-c++ git glib2 glib2-devel hmaccalc keyutils-libs-devel krb5-devel ksh libattr-devel libblkid-devel libselinux-devel libtool libuuid-devel lsscsi make ncurses-devel net-snmp-devel net-tools newt-devel numactl-devel parted patchutils pciutils-devel perl-ExtUtils-Embed pesign python3-devel redhat-rpm-config rpm-build systemd-devel tcl tcl-devel tk tk-devel wget xmlto yum-utils zlib-devel
-dnf install --enablerepo=devel install -y libyaml-devel
-dnf install --enablerepo=powertools install -y device-mapper-devel
+dnf --enablerepo=devel install -y libyaml-devel
+dnf --enablerepo=powertools install -y device-mapper-devel
