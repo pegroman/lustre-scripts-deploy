942a282b6ab4345a4e37180c61342f91c1944736
pegroman <roman.alejandro.b@gmail.com>
2024-03-31 22:38:56 -0300

    agregandro dependencia en install_server_kmod.sh

--- install_server_kmod.sh
+++ install_server_kmod.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+yum --enablerepo=devel install -y resource-agents
 yum --nogpgcheck --enablerepo=lustre-server install -y kmod-lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre lustre-resource-agents
