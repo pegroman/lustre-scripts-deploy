74af900e308547735353479a64d4e07597df2ee5
pegroman <roman.alejandro.b@gmail.com>
2024-03-31 21:00:18 -0300

    agregarndo dependencias y archivo repo de lustre

---
+++ depen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dnf install -y asciidoc audit-libs-devel automake bc binutils-devel bison device-mapper-devel elfutils-devel elfutils-libelf-devel expect flex gcc gcc-c++ git glib2 glib2-devel hmaccalc keyutils-libs-devel krb5-devel ksh libattr-devel libblkid-devel libselinux-devel libtool libuuid-devel libyaml-devel lsscsi make ncurses-devel net-snmp-devel net-tools newt-devel numactl-devel parted patchutils pciutils-devel perl-ExtUtils-Embed pesign python-devel redhat-rpm-config rpm-build systemd-devel tcl tcl-devel tk tk-devel wget xmlto yum-utils zlib-devel
